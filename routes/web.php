db13d8f32f2bf11963eaf495795cfadd7cfed69e
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2022-11-05 20:55:30 +0700

    menyelesaikan vidio ke 8

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@
 use App\Models\user\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-// use App\Models\Post;
+
+
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +39,19 @@ Route::get('/about', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
