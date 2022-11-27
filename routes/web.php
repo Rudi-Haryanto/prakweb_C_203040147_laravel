59f02e6d26f990976cb58fccc1c0c2677d1fa005
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2022-11-27 23:14:23 +0700

    menyelesaikan vidio 16 dan proses vidio 17

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
