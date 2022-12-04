0f551f0f9649291f0cefb96d048c073f9e93dc8c
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2022-12-04 17:49:47 +0700

    vidio 17

--- routes/web.php
+++ routes/web.php
@@ -78,4 +78,5 @@ Route::get('/dashboard', function(){
     return view('dashboard.index');
 })->middleware('auth');
 
+
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
